55737c720be1cb5a4de567b5a66682c6533f03fb
Serge Lebedev <angly.cat@gmail.com>
2019-06-19 03:17:50 +0600

    Remove unneeded React logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import 'normalize.css';
 import './App.css';
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
